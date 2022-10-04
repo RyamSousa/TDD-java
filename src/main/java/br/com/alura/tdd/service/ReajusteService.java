8a671d7d8eeaf0f7af2b2958a4d0513615446f2b
RyamSousa <ryam.sousa123@gmail.com>
2022-10-04 11:41:15 -0300

    :hammer: refact TDD step for 'reajuste' feature

--- src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -8,21 +8,10 @@ import java.math.RoundingMode;
 
 public class ReajusteService {
 
-    void reajuste(Funcionario funcionario, Desempenho desempenho){
-        if (desempenho == Desempenho.A_DESEJAR){
-            BigDecimal aumento = funcionario.getSalario()
-                    .multiply(new BigDecimal("0.03")).setScale(2, RoundingMode.HALF_UP);
-            funcionario.reajuste(aumento);
-        }
-        if (desempenho == Desempenho.BOM){
-            BigDecimal aumento = funcionario.getSalario()
-                    .multiply(new BigDecimal("0.15")).setScale(2, RoundingMode.HALF_UP);
-            funcionario.reajuste(aumento);
-        }
-        if (desempenho == Desempenho.OTIMO){
-            BigDecimal aumento = funcionario.getSalario()
-                    .multiply(new BigDecimal("0.20")).setScale(2, RoundingMode.HALF_UP);
-            funcionario.reajuste(aumento);
-        }
+    void reajuste(Funcionario funcionario, Desempenho desempenho) {
+        BigDecimal percentualReajuste = desempenho.percentualReajuste();
+        BigDecimal aumento = funcionario.getSalario()
+                .multiply(percentualReajuste);
+        funcionario.reajuste(aumento);
     }
 }
